7458e370dec6e4d34e9761c11f7b4e040020194d
Your Name <minamoyori19@outlook.jp>
2019-12-23 19:44:19 +0900

    good

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'home#top'
   get 'home/about'
-  resources :books
+  resources :books do
+  	resource :favorites, only: [:create, :destroy]
+  end
   devise_for :users, controllers: {
 	sessions: 'users/sessions',
 	registrations: 'users/registrations'
